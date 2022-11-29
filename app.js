464818c90bd6875694b41ee41d4ae4196ea5ddbb
julia-kretzer-senai <109150681+julia-kretzer-senai@users.noreply.github.com>
2022-11-29 11:29:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.get('', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+var cpf = ''
+
 app.get('/cliente', (req,res) => {
     res.sendFile(__dirname + '/cadastrar-cliente.html')
 })
@@ -32,30 +34,18 @@ app.get('/cliente', (req,res) => {
 app.get('/pedidos', (req,res) => {
     sql.query('select * from pedido', (err,results,fields) => {
         res.json(results)
-        var tabela = ''
-        tabela += `<table>`
-        tabela += `<tr><td>id</td><td>retirada_entrega</td><td>valor_total</td><td>data</td><td>hora</td><td>idSalgada</td>
-        <td>idDoce</td><td>idBebida</td><td>cpfCliente</td><td>cep</td></tr>`
-        for(var x in results) {
-            tabela += `<tr><td>${results[x].id}</td><td>${results[x].retirada_entrega}</td><td>${results[x].valor_total}</td><td>${results[x].data}</td><td>${results[x].hora}</td>
-            <td>${results[x].idSalgada}</td><td>${results[x].idDoce}</td><td>${results[x].idBebida}</td><td>${results[x].cpfCliente}</td><td>${results[x].cep}</td></tr>`
-        }
-        tabela += `</table>`
     })
 })
 
 app.get('/produtos', (req, res) => {
     sql.query('select * from produto', (err,results,fields) => {
         res.json(results)
-        var tabela = ''
-        tabela += `<table>`
-        tabela += `<tr><td>cod_barras</td><td>nome</td><td>tipo</td><td>quantidade</td></tr>`
-        for(var x in results) {
-            tabela += `<tr><td>${results[x].cod_barras}</td><td>${results[x].nome}</td><td>${results[x].tipo}</td><td>${results[x].quantidade}</td></tr>`
-        }
-        tabela += `</table>`
     })
 })
+
+// app.get('/produtos', (req,res) => [
+//     res.sendFile(__dirname + '/consulta-produtos.html')
+// ])
 
 app.get('/cadastrar-produto', (req,res) => {
     res.sendFile(__dirname + '/cadastrar-produto.html')
@@ -68,6 +58,7 @@ app.get('/finalizar', (req,res) => {
 app.post('/cadastro', (req,res) => {
     sql.query('insert into cliente (cpf, nome, sobrenome, nascimento, cep, uf, localidade, bairro, logradouro, numero, complemento) values (?,?,?,?,?,?,?,?,?,?,?)',
     [req.body.cpf, req.body.nome, req.body.sobrenome, req.body.nascimento, req.body.cep, req.body.uf, req.body.localidade, req.body.bairro, req.body.logradouro, req.body.num, req.body.complemento])
+    cpf = req.body.cpf
     res.redirect('/finalizar')
 })
 
@@ -78,8 +69,30 @@ app.post('/produtos', (req,res) => {
     res.redirect('/consulta')
 })
 
-app.post('/pizza', (req,res) => {
+var sabor = []
+var sabor1, sabor2, sabor3, sabor4, valorTotal
 
+app.post('/pizza', (req,res) => {
+    console.log(cpf)
+    tamanho = req.body.tamanho
+    sabor = req.body.sabor
+    let sabores = sabor.toString();
+    sabor1 = sabor.slice(0,1)
+    sabor2 = sabor.slice(1,2)
+    sabor3 = sabor.slice(2,3)
+    sabor4 = sabor.slice(3,4)
+    if (tamanho == 'p') {
+        valorTotal = 29.9
+    } else if (tamanho == 'm') {
+        valorTotal = 49.9
+    } else if (tamanho == 'g') {
+        valorTotal = 69.9
+    } else {
+        valorTotal = 99.9
+    }
+    sql.query('insert into pedido (valor, retiradaEntrega, sabores, tamanho) values(?,?,?,?,?)',
+    [valorTotal, req.body.entrega, sabores, tamanho])
+    console.log(sabor1, sabor2, sabor3, sabor4) // funcionando, precisa inserir no banco de dados
 })
 
 app.listen(port, () => {
